0dee277f6ef49adcfdea075d1b5a4a255968b5a4
Shailesh <shaileshsumanjha@gmail.com>
2018-05-10 11:19:18 -0600

    backup

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,13 +1,12 @@
 package io.pivotal.pal.tracker;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/time-entries")
@@ -15,37 +14,30 @@ public class TimeEntryController {
 
     private final CounterService counter;
     private final GaugeService gauge;
-
-    @Autowired
-    TimeEntryRepository timeEntryRepository;
-
-    @Autowired
-    ObjectMapper objectMapper;
+    private TimeEntryRepository timeEntriesRepo;
 
     public TimeEntryController(
             TimeEntryRepository timeEntriesRepo,
             CounterService counter,
             GaugeService gauge
     ) {
-        this.timeEntryRepository = timeEntriesRepo;
+        this.timeEntriesRepo = timeEntriesRepo;
         this.counter = counter;
         this.gauge = gauge;
     }
 
-    @ResponseBody
     @PostMapping
-    public ResponseEntity create(@RequestBody TimeEntry timeEntry){
-        TimeEntry createdTimeEntry = timeEntryRepository.create(timeEntry);
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry) {
+        TimeEntry createdTimeEntry = timeEntriesRepo.create(timeEntry);
         counter.increment("TimeEntry.created");
-        gauge.submit("timeEntries.count", timeEntryRepository.list().size());
+        gauge.submit("timeEntries.count", timeEntriesRepo.list().size());
 
         return new ResponseEntity<>(createdTimeEntry, HttpStatus.CREATED);
     }
 
-    @ResponseBody
     @GetMapping("{id}")
-    public ResponseEntity read(@PathVariable Long id){
-        TimeEntry timeEntry = timeEntryRepository.find(id);
+    public ResponseEntity<TimeEntry> read(@PathVariable Long id) {
+        TimeEntry timeEntry = timeEntriesRepo.find(id);
         if (timeEntry != null) {
             counter.increment("TimeEntry.read");
             return new ResponseEntity<>(timeEntry, HttpStatus.OK);
@@ -54,17 +46,15 @@ public class TimeEntryController {
         }
     }
 
-    @ResponseBody
     @GetMapping
-    public ResponseEntity list(){
+    public ResponseEntity<List<TimeEntry>> list() {
         counter.increment("TimeEntry.listed");
-        return new ResponseEntity<>(timeEntryRepository.list(), HttpStatus.OK);
+        return new ResponseEntity<>(timeEntriesRepo.list(), HttpStatus.OK);
     }
 
-    @ResponseBody
-    @PutMapping("/{id}")
-    public ResponseEntity update(@PathVariable Long id, @RequestBody TimeEntry timeEntry){
-        TimeEntry updatedTimeEntry = timeEntryRepository.update(id, timeEntry);
+    @PutMapping("{id}")
+    public ResponseEntity<TimeEntry> update(@PathVariable Long id, @RequestBody TimeEntry timeEntry) {
+        TimeEntry updatedTimeEntry = timeEntriesRepo.update(id, timeEntry);
         if (updatedTimeEntry != null) {
             counter.increment("TimeEntry.updated");
             return new ResponseEntity<>(updatedTimeEntry, HttpStatus.OK);
@@ -73,13 +63,12 @@ public class TimeEntryController {
         }
     }
 
-    @ResponseBody
-    @DeleteMapping("/{id}")
-    public ResponseEntity delete(@PathVariable Long id){
-        timeEntryRepository.delete(id);
+    @DeleteMapping("{id}")
+    public ResponseEntity<TimeEntry> delete(@PathVariable Long id) {
+        timeEntriesRepo.delete(id);
         counter.increment("TimeEntry.deleted");
-        gauge.submit("timeEntries.count", timeEntryRepository.list().size());
+        gauge.submit("timeEntries.count", timeEntriesRepo.list().size());
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
 }
